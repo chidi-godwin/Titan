0d849a2c534addf333784373d034330a606a69a9
gnc <chidieberen1998@gmail.com>
2020-06-10 11:47:39 +0100

    update config file to add database

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = 'KNOWYOURWORTH'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "postgresql:///mc_print"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql:///mc_print"
